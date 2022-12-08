b537d1642ac9c2b035793f2f5b5e79935b4aeca3
Roaa Talat <100158472+roaa0@users.noreply.github.com>
2022-12-08 21:46:58 +0200

    Update Assignment 3 Group Task.cpp

--- Assignment 3 Group Task.cpp
+++ Assignment 3 Group Task.cpp
@@ -21,22 +21,49 @@ int main()
 	//cout << rs4 << endl;
 	cout << *(rs5.begin()) << endl;
 	cout << *(rs5.end()) << endl;
-	
+
 	auto it = rs5.begin();
 	cout << it << endl;
 	cout << *it << endl;
-	it++; // built in 
+	it++; // built in
 	cout << it << endl;
 	cout << *it << endl;
 	rs5.erase(it);
 	cout << rs5 << endl;
 	RSVector<int>rs10(arr, 4);
-	// 1 5 6 10 
+	// 1 5 6 10
 	rs10.erase(rs10.begin()+1, rs10.end()-1);
 	cout << rs10 << endl;
 	RSVector<int>rs11(arr, 4);
 	//rs11.insert(rs11.begin(), 4);
 	//cout << rs11 << endl;
 
-}
 
+    cout<<"________________________________"<<endl;
+
+
+    RSVector<int>rs12(2),rs13(3);
+    rs12[0]=100;
+    rs12[1]=200;
+    rs12.push_back(13);
+    cout<<rs12.push_back(16)<<endl;
+    cout<<rs12.push_back(15)<<endl;
+    rs12.pop_back();
+    cout<<rs12<<endl;
+    cout<<rs12.get_size()<<endl;
+    cout<<rs12.resize()<<endl;
+    cout<<rs12.get_size()<<endl;
+    cout<<rs12<<endl;
+	
+    rs13[0]=100;
+    rs13[1]=200;
+    rs13[2]=13;
+    rs13.push_back(17);
+    cout<<rs13<<endl;
+    cout<<rs12.operator==(rs13)<<endl;
+    cout<<rs12.operator<(rs13)<<endl;
+    rs12.clear();
+    cout<<rs12.empty()<<endl;
+    cout<<rs13.empty()<<endl;
+    cout<<rs13.get_capacity()<<endl;
+}
