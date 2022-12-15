5ad619d81290a65a9876bd281311f74a35cc68ed
Salma. Mamdoh sabry <52897508+Salma-Mamdoh@users.noreply.github.com>
2022-12-15 12:43:26 +0200

    Update RSVector.h

--- RSVector.h
+++ RSVector.h
@@ -1,6 +1,4 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
-#include<bits/stdc++.h>
+#include<iostream>
 #include<iterator>
 #include <stdexcept>
 using namespace std;
@@ -15,40 +13,38 @@ template<typename T>
 class RSVector
 {
 private:
-	int size,capacity;
+	int size, capacity;
 	T* ptr;
 public:
 	RSVector(int x = 0);
 	RSVector(T* arr, int n);
 	RSVector(const RSVector& another);
 	RSVector<T>& operator=(const RSVector<T>& another);
-	RSVector<T>& operator=( RSVector<T>&& another);
-    T& operator[](int index);
-    int push_back(T data );
-    T pop_back();
+	RSVector<T>& operator=(RSVector<T>&& another);
+	T& operator[](int index);
+	void push_back(const T& data);
+	T pop_back();
 	typedef T* iterator;
 	T* begin();
 	T* end();
-    bool operator==(const RSVector<T>& another);
-    bool operator< (const RSVector<T>& another);
-	T*& operator++();
-	T* operator*();
-	iterator operator+(iterator it);
+	const T& operator*(iterator it) const;
+	T& operator++();
+	T& operator+(int x);
+	bool operator==(const RSVector<T>& another);
+	bool operator< (const RSVector<T>& another);
 	void erase(iterator it);
 	void erase(iterator it1, iterator it2);
 	void insert(iterator it, T x);
 	int get_size() const;
-    int get_capacity() const;
-    int resize();
-    bool empty();
-    void clear();
+	int get_capacity() const;
+	int resize();
+	bool empty();
+	void clear();
 	friend ostream& operator<< <T>(ostream& output, const RSVector <T>& another);
 	~RSVector();
 };
 
 #endif
-
-
 
 template<typename T>
 inline RSVector<T>::RSVector(int x)
@@ -68,8 +64,6 @@ inline RSVector<T>::RSVector(int x)
 		ptr = new T[size];
 	}
 
-
-
 }
 
 template<typename T>
@@ -80,7 +74,7 @@ inline RSVector<T>::RSVector(T* arr, int n)
 	capacity = size * sizeof(T);
 	for (int i = 0; i < size; i++) {
 		ptr[i] = arr[i];
-		cout << ptr[i] << endl;
+		//cout << ptr[i] << endl;
 	}
 }
 
@@ -144,76 +138,101 @@ inline T* RSVector<T>::begin()
 template<typename T>
 inline T* RSVector<T>::end()
 {
-	return &ptr[size-1];
+	return &ptr[size - 1];
 }
 
 template<typename T>
-inline T*& RSVector<T>::operator++()
+inline const T& RSVector<T>::operator*(iterator it) const
 {
-	// // O: insert return statement here
+	if ((it - ptr) >= 0 && (it - ptr) < size) {
+		return ptr[it - ptr];
+	}
+	else {
+		throw exception();
+	}
 }
 
 template<typename T>
-inline T* RSVector<T>::operator*()
+inline T& RSVector<T>::operator++()
 {
-	return nullptr;
+	
+	if (ptr++<=this->end()) {
+		ptr++;
+	cout << ptr << endl;
+	return *(ptr);
+	}
+	else {
+		throw exception();
+	}
 }
 
-
+template<typename T>
+inline T& RSVector<T>::operator+(int x)
+{
+	
+	if ((ptr+x) <= this->end()) {
+		ptr = ptr + x;
+		cout << ptr << endl;
+		return *(ptr);
+	}
+	else {
+		throw exception();
+	}
+}
 
 template<typename T>
 inline void RSVector<T>::erase(iterator it)
 {
-	// don't forget to check out of range
-
-	int pos = it-ptr;
-	for (int i = pos; i < size; i++) {
-		ptr[i] = ptr[i + 1];
+	int pos = it - ptr;
+	if (pos >= 0 && pos < size) {
+		for (int i = pos; i < size; i++) {
+			ptr[i] = ptr[i + 1];
+		}
+		size--;
 	}
-	size--;
+	else {
+		throw exception();
+	}
 }
 
 template<typename T>
 inline void RSVector<T>::erase(iterator it1, iterator it2)
 {
-	cout << "herr";
 	int pos1 = it1 - ptr;
 	int pos2 = it2 - ptr;
-	int diff = pos2 - pos1;
-	diff++;
-	for (int i = pos1; i < size; i++) {
-		ptr[i] = ptr[i + diff];
+	if ((pos1 >= 0 && pos1 < size) && ((pos2 >= 0 && pos2 < size))) {
+		int diff = pos2 - pos1;
+			diff++;
+			for (int i = pos1; i < size; i++) {
+				ptr[i] = ptr[i + diff];
+			}
+		size = size - diff;
 	}
-	size = size - diff;
+	else {
+		throw exception();
+	}
 }
 
 template<typename T>
 inline void RSVector<T>::insert(iterator it, T x)
 {
-	// don't forget to check the validation of input
-	/*
 	int pos = it - ptr;
-	int z = ptr[size - 1];
-	size++;
-	for (int i = size - 2; i >= pos; i--) {
-		ptr[i] = ptr[i - 1];
+	if (pos >= 0 && it<=this->end()) {
+		++size;
+		for (int i = size - 1; i > pos; i--) {
+			ptr[i] = ptr[i - 1];
+		}
+		ptr[pos] = x;
 	}
-	ptr[pos] = x;
-	ptr[size - 1] = z;
-	*/
-
+	else {
+		throw exception();
+	}
 }
 
 template<typename T>
 inline int RSVector<T>::get_size() const
 {
 	return size;
-}
-
-template<typename T>
-inline RSVector<T>::~RSVector()
-{
-	delete[] ptr;
 }
 
 template<typename T>
@@ -228,103 +247,112 @@ inline ostream& operator<<(ostream& output, const RSVector<T>& another)
 
 
 template<typename T>
-inline int RSVector<T>::push_back(T data) {
-    if(size == capacity){
-        T* newdata = new T[capacity*2];
-        for(int i = 0; i < size; i++){
-            newdata[i] = ptr[i];
-        }
-        delete[] ptr;
-        capacity*=2;
-        ptr = newdata;
-    }
-    ptr[size++] = data;
-    return size;
+void RSVector<T>::push_back(const T& data) {
+	if (size < capacity) {
+		ptr[size++] = data;
+	}
+	else {
+		cout << "Resizing to " << capacity * 2 << endl;
+		T* newdata = new T[capacity * 2];
+		capacity *= 2;
+
+		for (int i = 0; i < size; i++) {
+			newdata[i] = ptr[i];
+		}
+		delete[] ptr;
+		ptr = new T[size * 2];
+		ptr= newdata;
+		delete[] newdata;
+		ptr[size++] = data;
+	}
+
 }
 
 template<typename T>
-inline T RSVector<T>::pop_back(){
-    if(size>0){
-        size--;
-        return ptr[size];
-    }
+inline T RSVector<T>::pop_back() {
+	if (size > 0) {
+		size--;
+		return ptr[size];
+	}
+	else {
+		throw exception();
+	}
 }
 
 template<typename T>
 inline T& RSVector<T>::operator[](int index)
 {
-    if(index>=size){
-        cout<<"Error array index is out of range"<<endl;
-        exit(-1);
-    }
-    return ptr[index];
-    /*try{
-        if(index >= size)
-            throw;
-        cout<<"yes";
-        return ptr[index];
-    }
-    catch (...){
-        cout<<"Error array index is out of range"<<endl;
-        exit(0);
-    }*/
+	if (index < 0 || index>= size) {
+		throw exception();
+	}
+	return ptr[index];
+	
 }
 
 template<typename T>
 inline int RSVector<T>::get_capacity() const
 {
-    return capacity;
+	return capacity;
 }
 
 template<typename T>
 inline bool RSVector<T>::empty() {
-    if(size==0)
-        return true;
-    return false;
+	if (size == 0)
+		return true;
+	return false;
 }
 
 template<typename T>
 inline int RSVector<T>::resize() {
-    T* newdata = new T[capacity*2];
-    for(int i = 0; i < size; i++){
-        newdata[i] = ptr[i];
-    }
-    delete[] ptr;
-    capacity*=2;
-    ptr = newdata;
-    return size;
+	if (capacity < 2 * size) {
+		T* newptr = new T[capacity * 2];
+		for (int i = 0; i < size; i++) {
+			newptr[i] = ptr[i];
+		}
+		delete[] ptr;
+		capacity *= 2;
+		ptr = newptr;
+	}
+	size *= 2;
+	return size;
 }
 
 template<typename T>
 inline void RSVector<T>::clear()
 {
-    for(int i=0;i<size;i++)
-        ptr[i].~T();
-    size=0;
+	for (int i = 0; i < size; i++)
+		ptr[i].~T();
+	size = 0;
 }
 
 template<typename T>
 inline bool RSVector<T>::operator==(const RSVector<T>& another)
 {
-    if(size!=another.size) {
-        return false;
-    }
-    for(int i=0;i<size;i++){
-        if(ptr[i]!=another.ptr[i]){
-            return false;
-        }
-    }
-    return true;
+	if (size != another.size) {
+		return false;
+	}
+	for (int i = 0; i < size; i++) {
+		if (ptr[i] != another.ptr[i]) {
+			return false;
+		}
+	}
+	return true;
 }
 
 template<typename T>
 inline bool RSVector<T>::operator<(const RSVector<T>& another)
 {
-    for(int i=0;i<size;i++){
-        if(ptr[i]>another.ptr[i] ){
-            return false;
-        }
-    }
-    if(this->operator==(another))return false;
-    return true;
+	for (int i = 0; i < size; i++) {
+		if (ptr[i] > another.ptr[i]) {
+			return false;
+		}
+	}
+	if (this->operator==(another))return false;
+	return true;
 }
+template<typename T>
+inline RSVector<T>::~RSVector()
+{
+	//delete ptr;
+}
+
